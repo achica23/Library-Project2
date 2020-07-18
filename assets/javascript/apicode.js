f0e4a31a3e18152d4bef7477178ff55dfd8e2170
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-07-18 14:20:39 -0400

    working on bugs

--- assets/javascript/apicode.js
+++ assets/javascript/apicode.js
@@ -1,20 +1,18 @@
 $(document).ready(function(){
 //console.log("help")
 
-
-
-  //let key = "eCBC3foQULpuJ7el53IIVfVOjWDGl7cB"
   var queryURL = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=eCBC3foQULpuJ7el53IIVfVOjWDGl7cB";
   //console.log("it works")
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function(res){
-    //console.log(res)
-    const results = res.results
+  }).then(function random(res){
+    console.log("random word")
+    var results = res.results
     for (let i = 0; i < res.results.length; i++){
-      let books = $("<div class=\bookDiv\>")
-      var author = results[i].author;
+     let books = $("<div class=\bookDiv\>")
+
+     var author = results[i].author;
      var publisher = results[i].publisher;
      var publishDate = results[i].published_date;
      var genre = results[i].list_name;
@@ -31,9 +29,18 @@ $(document).ready(function(){
      $(".bookDiv").append(books);
     
     }
-  
+    return books
+    console.log(random())
+  });
+  var fun = random()
+
+  $(document).on("click", ".searchbar", function(userEntry) {
+    if (userEntry === fun){
+      let books = $("<div class=\bookDiv\>")
+    }
   })
 
+  
 
 
 
